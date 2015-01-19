05fd90d7e06fdcb4592f38413cb3ce7c7d6ef496
dragbone <ddragbone@gmail.com>
2015-01-19 21:15:33 +0100

    Added number variables

--- src/_.java
+++ src/_.java
@@ -1,20 +1,20 @@
 public class _ {
-	public static void main(String[] _) throws Throwable {
+	int _1 = 1, _2 = 2, _3 = _1 + _2, _7 = _1 << _2 ^ _3, _6 = _7 >> _1 << _1, _8 = _2 << _2, _9 = _1 ^ _8, _4 = _9 >> _1, _5 = _4 ^ _1, _0 = _5 >> _5;
+
+	public static void main(String[] _) throws Exception {
 		new _();
-		//p(Class.class.getMethods());
 	}
 
 	_() throws Exception {
-		//﹏(Class.class,0,"java.lang.System")
-		﹏(Class.forName("java.lang.System").getFields()[1].get(null), 3, "Hello World.");
+		﹏(((Class<?>) ﹏(Class.class, 0, "java.lang.System")).getFields()[_1].get(null), _3, "Hello World.");
 	}
 
 	Object ﹏(Object o, int i, Object... s) throws Exception {
 		return o.getClass().getMethods()[i].invoke(o, s);
 	}
-	
-	public static void p(Object[] os){
-		for(int i=0;i<os.length;++i)
+
+	public static void p(Object[] os) {
+		for (int i = 0; i < os.length; ++i)
 			System.out.println(os[i]);
 	}
 }
