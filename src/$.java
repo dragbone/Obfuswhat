36803257ffad24acf3dd3cb14cfa20a6b80c7cc1
dragbone <ddragbone@gmail.com>
2015-02-03 20:01:37 +0100

    Replaced some symbols and added smileys

--- src/$.java
+++ src/$.java
@@ -3,6 +3,14 @@ import java.lang.reflect.Method;
 public class $ {
 	public static void main(String[] args) {
 		printIndetifierStart();
+		
+		int v=0;
+		for(int i=1;i<=9;++i){
+			System.out.println(v);
+			v = v^i;
+		}
+		System.out.println(v);
+		System.out.println(v^1);
 		new $();
 	}
 
--- src/_.java
+++ src/_.java
@@ -1,41 +1,42 @@
 import java.lang.reflect.Method;
 
 public class _ {
-	int ¤ = new _[] { this }.length, 
-			¤¤ = ¤ << ¤, 
-			¤¤¤ = ¤ ^ ¤¤, 
-			¤¤¤¤ = ¤ << ¤¤, 
-			¤¤¤¤¤ = ¤¤¤¤ ^ ¤, 
-			¤¤¤¤¤¤ = ¤¤¤¤¤ ^ ¤¤¤, 
-			¤¤¤¤¤¤¤ = ¤ << ¤¤ ^ ¤¤¤, 
-			¤¤¤¤¤¤¤¤ = ¤¤ << ¤¤, 
-			¤¤¤¤¤¤¤¤¤ = ¤ ^ ¤¤¤¤¤¤¤¤, 
-			꠸ = ¤ & ¤¤;
+	_[] _ = {this};
+	int ︳ = _.length,
+			︳︳ = ︳ << ︳, 
+			︳︳︳ = ︳ | ︳︳, 
+			︳︳︳︳ = ︳ << ︳︳, 
+			︳︳︳︳︳ = ︳︳︳︳ | ︳, 
+			︳︳︳︳︳︳ = ︳︳︳︳︳ ^ ︳︳︳, 
+			︳︳︳︳︳︳︳ = ︳ << ︳︳ ^ ︳︳︳, 
+			︳︳︳︳︳︳︳︳ = ︳︳ << ︳︳, 
+			︳︳︳︳︳︳︳︳︳ = ︳ ^ ︳︳︳︳︳︳︳︳, 
+			︳︳︳︳︳︳︳︳︳︳ = ︳ ^ ︳︳ ^ ︳︳︳ ^ ︳︳︳︳ ^ ︳︳︳︳︳ ^ ︳︳︳︳︳︳ ^ ︳︳︳︳︳︳︳;
 	
 	public static void main(String[] _) throws Exception {
 		new _();
 	}
 
 	_() throws Exception {
-		Object obj = callCastClass(callCastClass(this, ¤¤¤¤¤¤¤), ¤¤¤¤¤¤¤¤¤);
-		Object sy = subSeq(call(getMethod(obj, 61), ¤¤¤¤¤), ¤¤, ¤¤¤¤);
-		Object st = subSeq(call(getMethod(obj, ¤¤¤¤), ¤¤¤¤¤), ¤¤¤¤, ¤¤¤¤¤¤);
-		Object em = subSeq(call(getMethod(obj, 60), ¤¤¤¤¤), ¤¤¤, ¤¤¤¤¤);
-		Object javalang = subSeq(getName(obj), ꠸, ¤¤¤¤¤¤¤¤ | ¤¤);
+		Object obj = callCastClass(callCastClass(this, ︳︳︳︳︳︳︳), ︳︳︳︳︳︳︳︳︳);
+		Object sy = subSeq(call(getMethod(obj, 61), ︳︳︳︳︳), ︳︳, ︳︳︳︳);
+		Object st = subSeq(call(getMethod(obj, ︳︳︳︳), ︳︳︳︳︳), ︳︳︳︳, ︳︳︳︳︳︳);
+		Object em = subSeq(call(getMethod(obj, 60), ︳︳︳︳︳), ︳︳︳, ︳︳︳︳︳);
+		Object javalang = subSeq(getName(obj), ︳︳︳︳︳︳︳︳︳︳, ︳︳︳︳︳︳︳︳ | ︳︳);
 		Object javaLangSystem = concat(concat(concat(javalang, sy), st), em);
-		call(((Class<?>) call(Class.class, ꠸, javaLangSystem)).getFields()[¤].get(null), ¤¤¤, "Hello World");
+		call(((Class<?>) call(Class.class, ︳︳︳︳︳︳︳︳︳︳, javaLangSystem)).getFields()[︳].get(null), ︳︳︳, "Hello World");
 	}
 
 	Object concat(Object ৲, Object ‿) throws Exception {
-		return call(৲, ¤¤¤¤¤ << ¤¤ | ¤¤¤¤¤¤¤, ‿);
+		return call(৲, ︳︳︳︳︳ << ︳︳ | ︳︳︳︳︳︳︳, ‿);
 	}
 
 	Object getName(Object obj) throws Exception {
-		return call(obj, ¤¤¤¤¤¤¤¤ | ¤¤¤);
+		return call(obj, ︳︳︳︳︳︳︳︳ | ︳︳︳);
 	}
 
 	Object subSeq(Object ৲, int a, int b) throws Exception {
-		return call(৲, ¤¤¤¤¤¤¤ << ¤¤¤ | ¤¤¤, a, b);
+		return call(৲, ︳︳︳︳︳︳︳ << ︳︳︳ | ︳︳︳, a, b);
 	}
 
 	Class<?> callCastClass(Object ⁀, int ‿) throws Exception {
@@ -49,6 +50,7 @@ public class _ {
 	}
 
 	Object call(Object ‿, int ⁀, Object... ৲) throws Exception {
+		௹:{break ௹;} // TURBAN SMILEYS!
 		return getMethod(‿, ⁀).invoke(‿, ৲);
 	}
 }
